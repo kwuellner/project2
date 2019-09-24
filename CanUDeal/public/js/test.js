2ccfd57f37555be648e754269dbbd973bd9eda10
darican <jcastillo8150@gmail.com>
2019-09-24 18:14:53 -0500

    images for vacations

---
+++ CanUDeal/public/js/test.js
@@ -0,0 +1,23 @@
+const {google} = require('googleapis');
+
+// Each API may support multiple version. With this sample, we're getting
+// v3 of the blogger API, and using an API key to authenticate.
+const blogger = google.blogger({
+  version: 'v3',
+  auth: 'AIzaSyAAWlIB2QfCTE824kwqoxndWXYAEefp1NI'
+});
+
+const params = {
+  blogId: 3213900
+};
+
+// get the blog details
+blogger.blogs.get(params, (err, res) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  }
+  console.log(`The blog url is ${res.data.url}`);
+});
+
+
